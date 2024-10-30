af9113e555d957f185280fd4f8455d647e25e97e
Diogo <diogo.barcelos@proappme.com.br>
2024-10-30 01:23:09 -0300

    feat: creating user request

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/d8barcelos/api-golang/src/configuration/rest_err"
+	"github.com/d8barcelos/api-golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("você chamou a rota errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
