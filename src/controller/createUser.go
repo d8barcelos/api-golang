5d8cbf1db3cd2bd0016ab8cbc99f1ad941cc3fdc
Diogo <diogo.barcelos@proappme.com.br>
2024-10-30 18:14:29 -0300

    feat: setting validation errors

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/d8barcelos/api-golang/src/configuration/rest_err"
+	"github.com/d8barcelos/api-golang/src/configuration/validation"
 	"github.com/d8barcelos/api-golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +12,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
